eb4b08508e394f2d710a2dadbf608f9b2a7e2189
luist1228 <36606927+luist1228@users.noreply.github.com>
2019-10-26 18:28:59 -0400

    Visualizacion y aumento de puntuacion, Transicion entre secciones del mundo

--- Juego/Assets/Enemy.cs
+++ Juego/Assets/Enemy.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
@@ -55,6 +53,7 @@ public class Enemy : MonoBehaviour
     {
         if (col.gameObject.tag.Equals("Bullet"))
         {
+            ScoreScript.scoreValue += 100;
             Destroy(enemigo);
         }
     }
---
+++ Juego/Assets/ScoreScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+    public static int scoreValue = 0;
+    public Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = "Score: ----" + scoreValue.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "Score: "+scoreValue.ToString();
+    }
+}
+
